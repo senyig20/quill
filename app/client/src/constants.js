8afbb4a861fabe2a37d64c4b90cb19a49b35293e
Yigit Sen <senyig.20@robcol.k12.tr>
2019-01-19 21:18:41 +0300

    Fixing an issue with the payment verification mode of the dashboard

--- app/client/src/constants.js
+++ app/client/src/constants.js
@@ -14,8 +14,8 @@ angular.module('reg')
         ADMITTED_AND_CANNOT_CONFIRM_TITLE: 'Son teyit tarihi olan [CONFIRM_DEADLINE] geçti.',
         ADMITTED_AND_CANNOT_CONFIRM: 'Kabul edilmene rağmen zamanında teyit etmedin.\nBu maalesef seni aramızda göremeyeceğiz anlamına geliyor.\nSeneye seni aramızda görme ümidiyle.',
         CONFIRMED_NOT_PAST_TITLE: 'Teyit formunu [CONFIRM_DEADLINE]\'a kadar düzenleyebilirsin',
-        PAYMENT: 'Ödemeni doğrulamamız lazım.',
-        PAYMENT_TITLE: 'Manuel bir kontrolden sonra ana sayfanda başvuru durumun gözükecek. Bir sıkıntı çıkarsa sana ulaşacağız.',
+        PAYMENT_TITLE: 'Ödemeni doğrulamamız lazım.',
+        PAYMENT: 'Manuel bir kontrolden sonra ana sayfanda başvuru durumun gözükecek. Bir sıkıntı çıkarsa sana ulaşacağız.',
         DECLINED: 'Bu yıl gelemeyeceğini duyduğumuz için üzüldük. Umarız seneye gelebilirsin!',
     })
     .constant('TEAM',{
