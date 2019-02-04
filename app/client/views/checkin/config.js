1ab57022287685ed8b3c713e9d60e88cd68d2f24
Yigit Sen <senyig.20@robcol.k12.tr>
2019-02-04 10:58:42 +0300

    Add new checkin page?

---
+++ app/client/views/checkin/config.js
@@ -0,0 +1,10 @@
+/* The name that'll be displayed on app */
+var APP_NAME = "Remixopolis Check-In";
+
+/*
+
+The registartion system's API endpoint without trailing forward-slash
+/orders /users and /auth/login are used.
+
+*/
+var API_ROOT = "https://quill123.herokuapp.com";
