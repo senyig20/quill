b0c4f54ee1f8513946e3dba98f3292a7c07ffe2a
Yigit Sen <senyig.20@robcol.k12.tr>
2019-02-04 10:51:25 +0300

    Revert checkin

--- app/client/views/checkin/js/index.js
+++ app/client/views/checkin/js/index.js
@@ -11,7 +11,22 @@ $.urlParam = function(name) {
 $(document).ready(function() {
   // Config stuff
   $(".header-text").text(APP_NAME);
-
+  function doLogin() {
+    $('.error').text('');
+    var username = $('#email').val();
+    var password = $('#password').val();
+    $.ajax({
+      type: "POST",
+      url: API_ROOT + '/auth/login',
+      data: {email: username, password: password},
+      success: function(data) {
+        storeJWT(data.token);
+        window.location.replace('home.html');
+      },
+      error: function() {
+        $('.error').text('Login failed!');
+      }
+    });
   // Get the JWT
   var jwt = $.urlParam('jwt');
   if (jwt == null && getStoredJWT() == undefined) {
