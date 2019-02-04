b8c8eee9a8859d823d5ee89c50d5ba168f541c0b
Yigit Sen <senyig.20@robcol.k12.tr>
2019-02-04 11:20:51 +0300

    Update new checkin

---
+++ app/client/views/checkin/checkinCtrl.js
@@ -0,0 +1,101 @@
+angular.module('reg')
+  .controller('CheckinCtrl',[
+    '$scope',
+    '$state',
+    '$stateParams',
+    'UserService',
+    function($scope, $state, $stateParams, UserService){
+
+      $scope.pages = [];
+      $scope.users = [];
+      $('.ui.dimmer').remove();
+      $scope.selectedUser = {};
+      $scope.selectedUser.sections = generateSections({status: '', confirmation: {
+        dietaryRestrictions: []
+      }, profile: ''});
+
+      function updatePage(data){
+        $scope.users = data.users;
+        $scope.currentPage = data.page;
+        $scope.pageSize = data.size;
+
+        var p = [];
+        for (var i = 0; i < data.totalPages; i++){
+          p.push(i);
+        }
+        $scope.pages = p;
+      }
+
+      UserService
+        .getPage($stateParams.page, $stateParams.size, $stateParams.query, $scope.statusFilters)
+        .success(function(data){
+          updatePage(data);
+        });
+
+      $scope.$watch('queryText', function(queryText){
+        UserService
+          .getPage($stateParams.page, $stateParams.size, queryText, $scope.statusFilters)
+          .success(function(data){
+            updatePage(data);
+          });
+      });
+      $scope.applyStatusFilter = function () {
+        UserService
+          .getPage($stateParams.page, $stateParams.size, $scope.queryText, $scope.statusFilters)
+          .success(function (data) {
+            updatePage(data);
+          });
+      };
+
+
+      $scope.goToPage = function(page){
+        $state.go('app.admin.users', {
+          page: page,
+          size: $stateParams.size || 100
+        });
+      };
+
+      $scope.goUser = function($event, user){
+        $event.stopPropagation();
+
+        $state.go('app.admin.user', {
+          id: user._id
+        });
+      };
+
+      $scope.toggleCheckIn = function($event, user, index) {
+        $event.stopPropagation();
+
+        if (!user.status.checkedIn){
+          swal({
+            title: "Whoa, wait a minute!",
+            text: "You are about to check in " + user.profile.name + "!",
+            type: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#DD6B55",
+            confirmButtonText: "Yes, check them in.",
+            closeOnConfirm: false
+            },
+            function(){
+              UserService
+                .checkIn(user._id)
+                .success(function(user){
+                  $scope.users[index] = user;
+                  swal("Accepted", user.profile.name + ' has been checked in.', "success");
+                });
+            }
+          );
+        } else {
+          UserService
+            .checkOut(user._id)
+            .success(function(user){
+              $scope.users[index] = user;
+              swal("Accepted", user.profile.name + ' has been checked out.', "success");
+            });
+        }
+      };
+
+
+      $scope.selectUser = selectUser;
+
+    }]);
