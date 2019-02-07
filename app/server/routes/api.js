269044816855e19f51af3af110639c04b21e25bd
Yigit Sen <senyig.20@robcol.k12.tr>
2019-02-07 22:36:57 +0300

    Revert all

--- app/server/routes/api.js
+++ app/server/routes/api.js
@@ -4,12 +4,6 @@ var request = require('request');
 
 jwt = require('jsonwebtoken');
 JWT_SECRET = process.env.JWT_SECRET;
-var express         = require('express');
-
-
-var app             = express();
-
-app.use(json2xls.middleware);
 
 var uuidv4 = require('uuid/v4');
 module.exports = function (router) {
