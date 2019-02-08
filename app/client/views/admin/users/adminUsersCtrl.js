184670b398bddd66ca1e67e753776073f0376cae
Yigit Sen <senyig.20@robcol.k12.tr>
2019-02-08 08:25:03 +0300

    n

--- app/client/views/admin/users/adminUsersCtrl.js
+++ app/client/views/admin/users/adminUsersCtrl.js
@@ -1,4 +1,3 @@
-
 angular.module('reg')
   .controller('AdminUsersCtrl',[
     '$scope',
@@ -270,8 +269,19 @@ angular.module('reg')
             output += "\n";
           }
 
+          var element = document.createElement('a');
+          element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(output));
+          element.setAttribute('download', "Remixopolis Export " + new Date().toDateString() + ".csv");
+          element.style.display = 'none';
+          document.body.appendChild(element);
+          element.click();
+          document.body.removeChild(element);
 
+<<<<<<< HEAD
 
+=======
+          });
+>>>>>>> parent of 63132d7... Add exportfromjson
       }
 
 
