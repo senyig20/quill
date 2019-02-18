0b2f8d67fb25ef8f03c1878f22839d05d09b80c9
Yigit Sen <senyig.20@robcol.k12.tr>
2019-02-18 21:52:26 +0300

    Cleaning up stats? and User

--- app/client/views/admin/stats/adminStatsCtrl.js
+++ app/client/views/admin/stats/adminStatsCtrl.js
@@ -4,25 +4,11 @@ angular.module('reg')
         '$http',
         'UserService',
         function ($scope, $http, UserService) {
-            $http
-                .get('/assets/schools.json')
-                .then(function (res) {
-                    $scope.schoolList = res.data;
-                });
 
             UserService
                 .getStats()
                 .success(function (stats) {
                     $scope.stats = stats;
-
-                    $scope.stats.demo.schools.forEach(function (school) {
-                        var schoolName = $scope.schoolList[school.email];
-
-                        if (schoolName) {
-                            school.email = school.email + ' (' + schoolName.school + ')';
-                        }
-                    })
-
                     $scope.loading = false;
                 });
 
