734465de8e882867366ae84d8e5f170397fc0c30
Yigit Sen <senyig.20@robcol.k12.tr>
2019-02-18 15:22:28 +0300

    Fixes & Fixes

--- app/client/views/admin/exportteams/adminExportTeamsCtrl.js
+++ app/client/views/admin/exportteams/adminExportTeamsCtrl.js
@@ -137,7 +137,7 @@ angular.module('reg')
 
                         var element = document.createElement('a');
                         element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(output));
-                        element.setAttribute('download', "Remixopolis Export " + new Date().toDateString() + ".csv");
+                        element.setAttribute('download', "Remixopolis Team Export " + new Date().toDateString() + ".csv");
                         element.style.display = 'none';
                         document.body.appendChild(element);
                         element.click();
