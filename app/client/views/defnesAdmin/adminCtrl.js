37b3818db743c57696e852a1513f71557c566846
Yigit Sen <senyig.20@robcol.k12.tr>
2019-04-26 07:11:15 +0300

    Revert "Defne's Pages - Add'em"
    This reverts commit a5db1eac34761d4229f03265d691fd8faddfc8ef.

--- app/client/views/defnesAdmin/adminCtrl.js
+++
@@ -1,7 +0,0 @@
-angular.module('reg')
-    .controller('DAdminCtrl', [
-        '$scope',
-        'UserService',
-        function ($scope, UserService) {
-            $scope.loading = true;
-        }]);
