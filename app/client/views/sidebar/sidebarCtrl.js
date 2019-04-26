bf1b6203048c5e9ecdfdc20ab0c4a4f0216e85dd
Yigit Sen <senyig.20@robcol.k12.tr>
2019-04-26 12:21:40 +0300

    Revert "Hopefully final version"
    This reverts commit 896989919defa9a40d59d6da94aab03138a609ee.

--- app/client/views/sidebar/sidebarCtrl.js
+++ app/client/views/sidebar/sidebarCtrl.js
@@ -25,24 +25,6 @@ angular.module('reg')
                 $scope.showSidebar = !$scope.showSidebar;
             };
 
-            $scope.isDefneSaylik = function (user) {
-                if (user.email == "senyig.20@robcol.k12.tr"){
-                    return true;
-                }
-                else{
-                    return false;
-                }
-            }
-            $scope.isYigitSen = function (user) {
-                if (user.email == "saydef.20@robcol.k12.tr"){
-                    return true;
-                }
-                else{
-                    return false;
-                }
-            }
-
-
             // oh god jQuery hack
             $('.item').on('click', function () {
                 $scope.showSidebar = false;
