e2c779e20e77bc80f9bb95eac9a2c4176ef95736
Yigit Sen <senyig.20@robcol.k12.tr>
2019-05-08 09:59:45 +0300

    Defne's Pages - Add'em

--- app/server/services/email.js
+++ app/server/services/email.js
@@ -11,7 +11,7 @@ const HACKATHON_NAME = process.env.HACKATHON_NAME;
 const EMAIL_ADDRESS = process.env.EMAIL_ADDRESS;
 const TWITTER_HANDLE = process.env.TWITTER_HANDLE;
 const FACEBOOK_HANDLE = process.env.FACEBOOK_HANDLE;
-const SENDGRID_API_KEY = process.env.SENDGRID_API_KEY;
+const API_KEY = process.env.SENDGRID_API_KEY;
 
 const EMAIL_CONTACT = process.env.EMAIL_CONTACT;
 const EMAIL_HEADER_IMAGE = process.env.EMAIL_HEADER_IMAGE;
@@ -23,7 +23,7 @@ const NODE_ENV = process.env.NODE_ENV;
 
 const options = {
     auth: {
-        api_key: SENDGRID_API_KEY
+        api_key: API_KEY
     }
 };
 
