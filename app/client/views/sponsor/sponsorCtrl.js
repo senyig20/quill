beaea9788f909696de1315ee425f74a7ed5d25c7
Yigit Sen <senyig.20@robcol.k12.tr>
2019-05-15 21:50:34 +0300

    Defne's Pages - Add'em

--- app/client/views/sponsor/sponsorCtrl.js
+++ app/client/views/sponsor/sponsorCtrl.js
@@ -21,7 +21,6 @@ angular.module('reg')
             function _updateUser() {
 
                 $scope.user.confirmation.sponsorSelected = true;
-                $scope.user.confirmation.signatureLiability = $scope.wshop1 + " " + $scope.wshop2 + " " + $scope.wshop3;
                 const confirmation = $scope.user.confirmation;
                 // Get the dietary restrictions as an array
 
